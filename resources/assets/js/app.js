e39679d0d92b068c7913ac3de3186cf884a1edc6
Elvin <elvis.mahmudov@gmail.com>
2015-08-31 16:35:27 +0000

    fix nav-bar

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,42 +1,50 @@
 (function ($) {
     var module = {
-       init: function (data) {
-           this.container = data.container;
-           this.effect = data.effect;
-           this.speed = data.speed || 200;
-           this.toggleClass = data.toggleClass;
-           this.subMenu = data.subMenu;
-           this.when = data.when;
-           this.done = data.done;
-           this.doEffect();
-       },
-       doEffect: function () {
-           var self = this;
-           this.container[self.when](function (e) {
-                var child;
-                e.preventDefault();
+     init: function (data) {
+         this.container = data.container;
+         this.effect = data.effect;
+         this.speed = data.speed || 200;
+         this.toggleClass = data.toggleClass;
+         this.subMenu = data.subMenu;
+         this.triggerOn = data.triggerOn;
+         this.done = data.done;
+         this.doEffect();
+     },
+     doEffect: function () {
+         var self = this;
+         this.container[self.triggerOn](function (e) {
+            var child;
+            e.preventDefault();
 
-                child = $(this).children(self.subMenu);
+            child = $(this).children(self.subMenu);
 
-                child[self.effect](self.speed);
+            child[self.effect](self.speed);
 
-                child[self.when](function(e) {
-                    e.stopPropagation();
-                })
-           });
-       }
-    }
+            child[self.triggerOn](function(e) {
+                e.stopPropagation();
+            })
 
-    module.init({
-       container: $('.parent'),
-       subMenu: 'ul',
-       effect: 'slideToggle',
-       speed: 400,
-       toggleClass: 'active',
-       when: 'click'
-    });
+            if ($.isFunction(self.done)) {
+                self.done.call(this);
+            }
+        });
+     }
+ }
 
-    $.material.init();
+ module.init({
+     container: $('.parent'),
+     subMenu: 'ul',
+     effect: 'slideToggle',
+     speed: 400,
+     toggleClass: 'active',
+     triggerOn: 'click',
+     done: function() {
+                //toggle caret class 
+                $(this).find('>a>.caret-right').toggleClass('caret-right-down')
+            }
+        });
 
-    $('.parent').children('ul').hide();
+ $.material.init();
+
+ $('.parent').children('ul').hide();
 })(jQuery)
